d7e5d8b07c52f8dd8b70268f306f802ac0e711a8
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-08-25 19:00:11 -0500

    Clase 1

---
+++ Clase 1/index.js
@@ -0,0 +1,52 @@
+//comentario de una sola linea
+
+/* 
+un comentario
+de
+varias
+lineas
+*/
+//forma no recomendada de declarar variables
+/* var nombre = "camila";
+
+//declaracion de una variable
+let edad = 45;
+const ALTURA = 180;
+let numeroUnoBonito;
+
+console.log("Aca la altura", ALTURA); */
+
+/* let numeroUno = 5;
+let numeroDos = 10;
+
+let suma = numeroUno + numeroDos;
+let resta = numeroUno - numeroDos;
+let multiplicacion = numeroUno * numeroDos;
+let division = numeroUno / numeroDos;
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division); */
+
+/* let nombre = "andres";
+let saludo = "Hola";
+
+let mensaje = saludo + " " + nombre + " todo bien que hace?";
+console.log(mensaje);
+
+
+let mensaje1 = `${saludo} ${nombre} todo bien que hace?`;
+console.log(mensaje1); */
+
+/* let nombre = prompt("Ingresa tu nombre");
+alert(nombre); */
+
+/* let numero1 = parseInt(prompt("Ingrese un numero 1"));
+let numero2 = parseInt(prompt("Ingrese un numero 2"));
+let suma = numero1 + numero2;
+
+alert(suma); */
+
+let nombre = "andres";
+console.log(nombre);
