41f733967ae050e089946d531c7695a889bf8d87
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-09-02 16:49:56 -0500

    After 1

---
+++ After 1/index.js
@@ -0,0 +1,25 @@
+/* let nombre = "marina";
+
+if(nombre === "marina"){
+  console.log("hola marina");
+}else{
+  console.log("no eres marina");
+} */
+
+/* let edad = prompt("Ingrese su edad");
+
+while (edad != "ESC") {
+  if (parseInt(edad) < 15) {
+    alert("No puedes entrar");
+  } else if (parseInt(edad) < 18) {
+    alert("puedes entrar con un adulto");
+  } else {
+    alert("Puedes entrar");
+  }
+
+  edad = prompt("Ingrese su edad");
+} */
+
+for(const letra of "hola"){
+  console.log(letra);
+}
