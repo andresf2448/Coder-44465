b450b96fc8c76602bc92f62d456f2a6392bf376c
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-09-13 18:10:45 -0500

    Clase 6

---
+++ Clase 6/index.js
@@ -0,0 +1,61 @@
+// const arregloVacio = [];
+// const numeros = [1, 2, 3, 2, 4, 5, 3];
+// const nombres = ["camila", "andres", "mariana", "pedro", "mario", "yuliana"];
+// const booleanos = [true, false];
+// const varios = [1, "gabriel", true];
+
+// console.log(numeros[2] + numeros[5]);
+
+// for (let index = 0; index < nombres.length; index++) {
+//   const elemento = nombres[index];
+//   console.log(index, elemento);
+// }
+
+// nombres[0] = "mateo";
+// console.log(nombres);
+
+/* const nombres = ["camila", "andres"];
+nombres.push("Pedro"); //agrega elementos al final del arreglo
+console.log(nombres);
+
+nombres.unshift("Yuliana");//agregar elementos al principio del arreglo
+console.log(nombres); */
+
+/* const nombres = ["camila", "andres"];
+nombres.pop(); //sacar elemento del final
+console.log(nombres); */
+
+/* const nombres = ["camila", "andres"];
+nombres.shift(); //sacar elemento del inicio del arreglo
+console.log(nombres); */
+
+/* const nombres = ["camila", "andres", "mariana", "pedro", "mario", "yuliana"];
+nombres.splice(1, 3);// elimina desde la posicion dada y la cantidad dada */
+
+/* const nombres = ["camila", "andres", "mariana", "pedro", "mario", "yuliana"];
+nombres.splice(1, 0, "gabriel");// agrega elemento en la posicion pasada como primer parametro */
+// console.log(nombres);
+
+/* const nombres = ["camila", "andres", "mariana", "pedro", "mario", "yuliana"];
+console.log(nombres.join("")); //une todos los elementos de un arreglo en un string y los separa por el parametro pasado
+ */
+
+/* const nombres = ["camila", "andres", "mariana", "pedro", "mario", "yuliana"];
+const perritos = ["firu", "zeus", "max"];
+
+const varios = perritos.concat(nombres); //une dos arreglos en uno solo
+console.log(varios); */
+
+/* const nombres = ["camila", "andres", "mariana", "pedro", "mario", "yuliana"];
+const fragmento = nombres.slice(1, 5);// genera una copia de un fragmento del arreglo
+console.log(fragmento); */
+
+/* const nombres = ["camila", "andres", "mariana", "pedro", "mario", "yuliana"];
+console.log(nombres.indexOf("mario")); //busca la posicion del elemento pasado por parametro */
+
+/* const nombres = ["camila", "andres", "mariana", "pedro", "mario", "yuliana"];
+console.log(nombres.includes("pedro")); */
+
+/* const nombres = ["camila", "andres", "mariana", "pedro", "mario", "yuliana"];
+nombres.reverse();
+console.log(nombres); */
