69ddd09160f67e922ec8bc07de30067f4ad54cf6
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-09-27 17:45:42 -0500

    Clase 10

---
+++ Clase 10/index.js
@@ -0,0 +1,34 @@
+/*
+//para guardar info en el storage
+localStorage.setItem("nombre", "fabian");
+localStorage.setItem("numero", "5");
+localStorage.setItem("valor", "true");
+ */
+
+/* 
+//traer la informacion del storage
+let mensaje = localStorage.getItem("numero");
+let numero = localStorage.getItem("nombre");
+let valor = localStorage.getItem("valor");
+
+console.log(mensaje);
+console.log(numero);
+console.log(valor); */
+
+// sessionStorage.setItem("nombre", "juan");
+
+/* localStorage.setItem("objeto", {nombre: "andres", edad: 70});
+localStorage.setItem("arreglo", [2,3,4,5,4]); */
+
+/* for(let i = 0; i < localStorage.length; i++){
+  let clave = localStorage.key(i);
+  console.log("clave", clave);
+  console.log("valor", localStorage.getItem(clave));
+} */
+
+/* //para eliminar por item colocamos removeItem("clave");
+localStorage.removeItem("nombre");
+sessionStorage.removeItem("edad");
+//para eliminar el storage completo hacemos clear();
+localStorage.clear();
+sessionStorage.clear(); */
