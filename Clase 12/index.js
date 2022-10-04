071d3cccbf3a233daf1d5c6708506e7cdb4d517c
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-10-04 18:44:28 -0500

    Clase 12

--- Clase 12/index.js
+++ Clase 12/index.js
@@ -94,7 +94,7 @@ console.log(usuario1 || "el usuario no existe"); */
 
 // const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-const usuario = {
+/* const usuario = {
   nombre: "andres",
   edad: 19,
   telefono: {
@@ -103,4 +103,130 @@ const usuario = {
   }
 };
 
-console.log(usuario?.telefono?.trabajo || "El usuario no existe");
+console.log(usuario?.telefono?.trabajo || "El usuario no existe"); */
+
+//DESESTRUCTURACION
+/* const usuario = {
+  nombre: "juan",
+  edad: 45,
+  direccion: "AV"
+}; */
+
+// let nombre = usuario.nombre;
+// let edad = usuario.edad;
+// let direccion = usuario.direccion;
+
+/* let {
+  nombre,
+  edad,
+  direccion
+} = usuario;
+console.log(nombre, edad); */
+
+/* const usuario = {
+  nombre: "juan",
+  edad: 45,
+  direccion: "AV",
+  telefono: {
+    cel: 1234234,
+    casa: 234234,
+    trabajo: 2134234,
+  },
+}; */
+
+/* let {
+  nombre,
+  telefono: { cel },
+} = usuario;
+
+console.log(cel); */
+
+/* const usuario = {
+  nombre: "juan",
+  edad: 45,
+  direccion: "AV",
+};
+
+let { nombre: nombrePersona, edad } = usuario;
+console.log(nombrePersona); */
+
+/* const mostrar = ({nombre, edad, direccion}) => {
+  console.log(nombre, edad, direccion);
+};
+
+const usuario = {
+  nombre: "juan",
+  edad: 45,
+  direccion: "AV",
+};
+
+mostrar(usuario); */
+
+/* const nombres = ["Juan", "Julieta", "Carlos", "Mariela"];
+const [a, , b] = nombres;
+console.log(a, b); */
+
+/* const nombres = ["Juan", "Julieta", "Carlos", "Mariela"];
+console.log(...nombres);
+console.log(nombres.join(" ")); */
+
+/* const numeros = [4, 77, 92, 10, 3, -32, 54, 11];
+console.log(Math.max(...numeros)); */
+
+/* let nombre = "andres";
+let nombre1 = nombre;
+
+nombre1 = "juan";
+console.log(nombre);
+
+|"andres"|        | "juan" | 
+nombre            nombre1 */
+
+/* let objeto = {nombre: "juan", edad: 50};
+let objeto1 = {...objeto};
+
+objeto1.nombre = "Mariano";
+
+console.log(objeto);
+
+| {nombre: "juan", edad: 50} |    | {nombre: "Mariano", edad: 50} |
+    objeto                             objeto1
+
+
+
+
+
+const usuario = {
+  nombre: "juan",
+  edad: 45,
+  direccion: "AV",
+};
+
+const usuario1 = {
+  ...usuario,
+  nombre: "mariano",
+  papa: "fabian"
+}
+
+console.log(usuario1); */
+
+/* class Persona{
+  constructor(nombre, edad){
+    this.nombre = nombre;
+    this.edad = edad;
+  }
+}
+let personas = [];
+function valida(e){
+  e.preventDefault();
+
+  let inputs = e.target.children;
+  let persona = new Persona(inputs[0].value, inputs[1].value)
+  personas.push(persona);
+} */
+
+const mostrar = (...numeros) => {
+  return numeros.reduce((acum, item) => acum + item, 0);
+};
+
+console.log(mostrar(1, 3, 3, 56, 67, 7, 8, 98, 5, 44, 2, 43434));
